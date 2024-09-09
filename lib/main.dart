ced0242b699392a912c90cb3dffbe4575f06dbf0
Melvim Rafael <103382153+MelvimRafael@users.noreply.github.com>
2024-09-09 16:21:20 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,20 @@ import 'package:contas/screens/login_screen.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-// Certifique-se de que o caminho está correto
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:awesome_dialog/awesome_dialog.dart';
 
 Future<bool> isAuthenticated() async {
   final storage = FlutterSecureStorage();
   final token = await storage.read(key: 'token');
-  return token != null; // Retorna verdadeiro se o token estiver presente
+  return token != null;
 }
 
 void main() {
   runApp(DevicePreview(
-    enabled: true, // Habilita o Device Preview
-    builder: (context) => MyApp(), // Constrói o aplicativo
+    enabled: true,
+    builder: (context) => MyApp(),
   ));
 }
 
@@ -25,6 +27,29 @@ class MyApp extends StatelessWidget {
       title: 'Contas Compartilhadas',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold, color: Colors.blue.shade900),
+          titleLarge: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic, color: Colors.blue.shade700),
+          bodyMedium: TextStyle(fontSize: 14.0, color: Colors.blue.shade600),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.grey.shade200,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue.shade600,
+            padding: EdgeInsets.symmetric(vertical: 15),
+            textStyle: TextStyle(fontSize: 18),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
       ),
       home: AuthCheck(),
     );
@@ -42,9 +67,9 @@ class AuthCheck extends StatelessWidget {
             body: Center(child: CircularProgressIndicator()),
           );
         } else if (snapshot.hasData && snapshot.data == true) {
-          return DashboardScreen(); // Usuário autenticado
+          return DashboardScreen();
         } else {
-          return LoginScreen(); // Usuário não autenticado
+          return LoginScreen();
         }
       },
     );
